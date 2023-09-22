d307bf95fe983cbc261a0790ef9f08186d955cf6
Sarahkiss215 <skisiangani5@gmail.com>
2023-09-22 04:36:08 -0700

    Add _add function

---
+++ _add.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * _add - adds the top two elements of the stack.
+ * @head: stack head
+ * @i: number of lines
+ *
+ * Return: Nothing.
+*/
+void _add(stack_t **head, unsigned int i)
+{
+	stack_t *h;
+	int length = 0, next_n;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", i);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	next_n = h->n + h->next->n;
+	h->next->n = next_n;
+	*head = h->next;
+	free(h);
+}
