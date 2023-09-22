6d0349ad5a41e9ded8038f94e2f5dfaadf7f1c50
Sarahkiss215 <skisiangani5@gmail.com>
2023-09-22 04:47:32 -0700

    Add _nop function

---
+++ _nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+  * _nop- nothing
+  * @head: stack head
+  * @i: number of lines
+  *
+  * Return: Nothing.
+ */
+void _nop(stack_t **head, unsigned int i)
+{
+	(void) i;
+	(void) head;
+}
