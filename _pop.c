a41bca245befda0ddf12f843ec5d37f8724657f1
Sarahkiss215 <skisiangani5@gmail.com>
2023-09-22 04:11:09 -0700

    Add _pop function

---
+++ _pop.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * _pop - prints the top
+ * @head: stack head
+ * @i: number of lines
+ *
+ * Return: Nothing.
+*/
+void _pop(stack_t **head, unsigned int i)
+{
+	stack_t *h;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", i);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	*head = h->next;
+	free(h);
+}
