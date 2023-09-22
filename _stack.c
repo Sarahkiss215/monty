b131d5b9db06e3498bc0ec043f9e0f227a5ab769
Sarahkiss215 <skisiangani5@gmail.com>
2023-09-22 06:50:50 -0700

    Add _stack function

---
+++ _stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * _stack - prints the top
+ * @head: head of stack
+ * @i: number of lines
+ *
+ * Return: Nothing.
+*/
+void _stack(stack_t **head, unsigned int i)
+{
+	(void)head;
+	(void)i;
+	info.lifi = 0;
+}
