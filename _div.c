e25de15d09e0a1090bec9ecbf0a6edadc5752dc2
Sarahkiss215 <skisiangani5@gmail.com>
2023-09-22 05:13:00 -0700

    Add _div function

---
+++ _div.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+
+/**
+ * _div - divides the top two elements of the stack.
+ * @head: head of the stack
+ * @i: number of lines
+ *
+ * Return: Nothing.
+*/
+void _div(stack_t **head, unsigned int i)
+{
+	stack_t *h;
+	int length = 0, new_n;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", i);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", i);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	new_n = h->next->n / h->n;
+	h->next->n = new_n;
+	*head = h->next;
+	free(h);
+}
