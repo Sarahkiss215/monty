cb4dc15797014b4cb7b893c38afbdedc2507cba4
Sarahkiss215 <skisiangani5@gmail.com>
2023-09-22 04:28:49 -0700

    Ad _swap function

---
+++ _swap.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ * _swap - adds the top two elements of the stack.
+ * @head: stack head
+ * @i: number of lines
+ *
+ * Return: Nothing
+*/
+void _swap(stack_t **head, unsigned int i)
+{
+	stack_t *h;
+	int length = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", i);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->n;
+	h->n = h->next->n;
+	h->next->n = aux;
+}
